98e03b997e75ffb9abff4c17fe31c0da1ee9366a
Lucero07 <ing.lucero.ramos@gmail.com>
2017-04-04 08:46:52 -0500

    segundo intento

--- main.js
+++ main.js
@@ -6,16 +6,43 @@ function agreagarTwitter() {
     } else if (autor === '') {
         alert("No estas agregando el Autor?");
     } else {
-        var textos = "Tweeet: " + texto + "<br>" + "Autor: " + autor;
+        var textos = "Tweeet: " + texto + "<br>" + "Autor: " + autor + "<hr>";
         var article = document.createElement('article');
+      
 
         article.innerHTML = textos === '' ? '' : textos
-        document.getElementById('lista_comentarios').appendChild(article);
+              document.getElementById('lista_comentarios').appendChild(article);
+
+        //Limpiar las entradas
         document.getElementById('comentarios').value = "";
         document.getElementById('autor').value = "";
-    }
+        document.getElementById("num_caracteres").innerHTML = 140;
+      };
 };
 
-function cuenta() {
-    document.article[0].caracteres.value = document.article[0].texto.value.length;
+var contador = document.getElementById('comentarios');
+contador.addEventListener('keypress', contadorCaracteres);
+var inicio = 140;
+
+function contadorCaracteres() {
+    if (inicio > 0) {
+        inicio -= 1;
+        var numCaracteres = document.getElementById("num_caracteres");
+        numCaracteres.innerHTML = inicio;
+    } else {
+        alert("No puedes escribir mas! :(");
+    };
 };
+
+
+
+document.getElementById('contador_click').innerHTML =0;
+var contaClick = document.getElementById('contador_click');
+window.addEventListener('click',contadorClick);
+var inicioClick = 0;
+
+
+function contadorClick() {
+inicioClick+=1;
+
+}
